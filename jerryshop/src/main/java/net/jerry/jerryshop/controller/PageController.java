9b4ebe09e991dbab25007f7dce0cc9fc218818eb
jerrycheng <236303933@qq.com>
2018-08-12 22:14:50 +0800

    successfully load static resources, use bootstrap 4.1.3. not 3.3.7

--- jerryshop/src/main/java/net/jerry/jerryshop/controller/PageController.java
+++ jerryshop/src/main/java/net/jerry/jerryshop/controller/PageController.java
@@ -16,29 +16,5 @@ public class PageController {
 		
 		return mv;
 	}
-/*	
-	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value="greeting", required = false)String greeting) {
-		if(greeting == null) {
-			greeting ="Hello there";
-		}
-		
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
-		
-		return mv;
-	}	
-*/
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting) {
-		if(greeting == null) {
-			greeting ="Hello there";
-		}
-		
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
-		
-		return mv;
-	}	
 	
 }
